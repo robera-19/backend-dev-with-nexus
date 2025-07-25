d13be247ffcac4a3899a5a86d4afe3a3fc7655f0
robera <robarobera50@gmail.com>
2025-07-25 01:42:48 -1200

    chore: package manager added

---
+++ node-basics/package_manager/manager.js
@@ -0,0 +1,14 @@
+// Package manager
+// resolve dependencies, for library install
+// Linux -> sudo apt install <something>
+// control versions
+// Example: npm -> node package manager
+// Example: bun, yarn
+// cmd:
+//1. npm init
+// better -> npm init -y
+// 2. npm install <library>
+// 3. npm install
+// npx -> when we talk about ORM
+// https://www.npmjs.com/package/@better-auth/utils?activeTab=readme
+//
