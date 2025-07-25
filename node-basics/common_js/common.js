ffe24464711a89ceada1968e2bbf62c71ed3b39e
robera <robarobera50@gmail.com>
2025-07-25 01:29:16 -1200

    feat: add common js import method

---
+++ node-basics/common_js/common.js
@@ -0,0 +1,9 @@
+// Use COmmonJS Import it
+//
+const BubbleSOrt = require("./sorting.js");
+console.log(BubbleSOrt);
+
+let unsorted_arr = [2, 3, 1, 5, 4];
+console.log(BubbleSOrt(unsorted_arr));
+// [1,2,3,4,5]
+//
